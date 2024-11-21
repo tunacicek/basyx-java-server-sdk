80aeeb8fe3d8077ce2c1b3e5a3513915753d9e26
Tunahan Cicek <tunahan.cicek@de.bosch.com>
2024-11-21 09:29:55 +0100

    Prepare Testcases

---
+++ basyx.aasregistry/basyx.tractusxregistry-testcases/src/test/java/org/eclipse/digitaltwin/basyx/tractusxregistry/testcases/helper/TenantFactory.java
@@ -0,0 +1,38 @@
+package org.eclipse.digitaltwin.basyx.tractusxregistry.testcases.helper;
+
+import lombok.Value;
+
+public class TenantFactory {
+
+   private static final String TENANT_ONE = "TENANT_ONE";
+   private static final String TENANT_TWO = "TENANT_TWO";
+   private static final String TENANT_THREE = "TENANT_THREE";
+
+   private final Tenant tenantOne;
+   private final Tenant tenantTwo;
+   private final Tenant tenantThree;
+
+   public TenantFactory( String publicClientId ) {
+      this.tenantOne = new Tenant( publicClientId, TENANT_ONE );
+      this.tenantTwo = new Tenant( publicClientId, TENANT_TWO );
+      this.tenantThree = new Tenant( publicClientId, TENANT_THREE );
+   }
+
+   public Tenant tenantOne() {
+      return tenantOne;
+   }
+
+   public Tenant tenantTwo() {
+      return tenantTwo;
+   }
+
+   public Tenant tenantThree() {
+      return tenantThree;
+   }
+
+   @Value
+   public static class Tenant {
+      String publicClientId;
+      String tenantId;
+   }
+}
